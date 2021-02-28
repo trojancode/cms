0706eaa46426ca6d85772d4306696c939e51a478
trojancode <codeboycodes@gmail.com>
2021-02-28 03:17:25 -0800

    error fix

--- user-service/handler.js
+++ user-service/handler.js
@@ -2,10 +2,9 @@
 
 const {  createUser , loginUser,getUser} = require("./controllers/user");
 const middy = require('middy')
-const { auth, jwt } = require("./middleware/auth");
+const { auth, jwt } = require("../middleware/auth");
 
 module.exports.createUser = middy(createUser).use(auth());
-
 
 module.exports.loginUser = middy(loginUser).use(jwt());
 
